72d54ba33513f9d8788a212ba0d9d54b35f25861
Angêlica Araújo <134542537+araujo-angel@users.noreply.github.com>
2024-01-16 16:42:59 -0300

    aula 16/01

--- src/aula5/aula5/HelloWord.java
+++ src/aula5/aula5/HelloWord.java
@@ -1,3 +1,5 @@
+package aula5;
+
 class HelloWord {
    public static void main(String[] args) {
         System.out.println("Hello Word");//comando de saida na tela
--- src/aula6/aula6/Argumentos.java
+++ src/aula6/aula6/Argumentos.java
@@ -1,3 +1,5 @@
+package aula6;
+
 class Argumentos {
     public static void main (String[] args) {
         System.out.println("Você digitou " + args[0]);
